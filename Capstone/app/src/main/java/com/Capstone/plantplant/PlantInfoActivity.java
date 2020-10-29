bb66aaf00a27962e8423f0800ab52552149a0249
jiyeung-yun <jiyoung3802@gmail.com>
2020-10-29 11:02:57 +0900

    식물 정보 액티비티 틀

--- Capstone/app/src/main/java/com/Capstone/plantplant/PlantInfoActivity.java
+++ Capstone/app/src/main/java/com/Capstone/plantplant/PlantInfoActivity.java
@@ -1,6 +1,11 @@
 package com.capstone.plantplant;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +16,24 @@ import androidx.appcompat.app.AppCompatActivity;
 * */
 public class PlantInfoActivity extends AppCompatActivity {
 
+    ImageButton btn_info_close;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+
         setContentView(R.layout.activtiy_plantinfo);
+
+        btn_info_close = findViewById(R.id.btn_info_close);
+        btn_info_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
